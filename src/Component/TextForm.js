1eb9c2cbeac72ae23cc7b3f7ce31810e5579f2fb
Akhilesh Kumar Sahu <118347309+mrakhil456@users.noreply.github.com>
2025-09-23 15:43:02 +0530

    Update TextForm.js

--- src/Component/TextForm.js
+++ src/Component/TextForm.js
@@ -60,7 +60,7 @@ return (
 </div>
 <div className="container"  style={{color:props.mode==='dark'?'white':'#180208ff'}} >
     <h2 >Your text summary</h2>
-    <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+    <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
     <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} minutes to read</p>
     <h2  >Preview</h2>
     <p>{text.length > 0 ? text : " Nothing to preview!"}</p>
@@ -68,5 +68,6 @@ return (
 </>
 )
 }
+
 
  
