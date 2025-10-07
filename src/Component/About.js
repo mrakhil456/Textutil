503da63b08dcef4bdb196eab3c978d2929149ade
Akhilesh Kumar Sahu <118347309+mrakhil456@users.noreply.github.com>
2025-10-08 00:30:40 +0530

    Update About.js

--- src/Component/About.js
+++ src/Component/About.js
@@ -4,7 +4,7 @@
            
         return (
 
-            <div className="container"   style={{backgroundColor:props.mode==='dark'?'#2f2b2cff':'rgba(5, 107, 147, 0)' , color:props.mode==='dark'?'white':'black'}}>
+            <div className="container"   style={{backgroundColor:props.mode==='dark'?'#191a1bff':'rgba(5, 107, 147, 0)' , color:props.mode==='dark'?'white':'black'}}>
 
                 <h1>About Us</h1>
                 <div>
@@ -13,40 +13,40 @@
                 </h6>
                 </div>
                  
-                <div className="accordion" id="accordionExample"  style={{backgroundColor:props.mode==='dark'?'#2f2b2cff':'rgba(5, 107, 147, 0)' , color:props.mode==='dark'?'white':'black'}}>
-        <div className="accordion-item" style={{backgroundColor:props.mode==='dark'?'#2f2b2cff':'rgba(5, 107, 147, 0)' , color:props.mode==='dark'?'white':'black'}}>
+                <div className="accordion" id="accordionExample"  style={{backgroundColor:props.mode==='dark'?'#191a1bff':'rgba(5, 107, 147, 0)' , color:props.mode==='dark'?'white':'black'}}>
+        <div className="accordion-item" style={{backgroundColor:props.mode==='dark'?'#191a1bff':'rgba(5, 107, 147, 0)' , color:props.mode==='dark'?'white':'black'}}>
             
             <h2 className="accordion-header">
-            <button className="accordion-button" type="button"  data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style={{backgroundColor:props.mode==='dark'?'#2f2b2cff':'rgba(5, 107, 147, 0)' , color:props.mode==='dark'?'white':'black'}}>
+            <button className="accordion-button" type="button"  data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style={{backgroundColor:props.mode==='dark'?'#191a1bff':'rgba(5, 107, 147, 0)' , color:props.mode==='dark'?'white':'black'}}>
                   <h5><strong>Analyze your Text</strong></h5>
             </button>
             </h2>
             <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-            <div className="accordion-body"  style={{backgroundColor:props.mode==='dark'?'#2f2b2cff':'rgba(5, 107, 147, 0)' , color:props.mode==='dark'?'white':'black'}}>
+            <div className="accordion-body"  style={{backgroundColor:props.mode==='dark'?'#191a1bff':'rgba(5, 107, 147, 0)' , color:props.mode==='dark'?'white':'black'}}>
                   It is a Simple Text Analyzer , which mutilates your text, but in a tender way
             </div>
             </div>
         </div>
-        <div className="accordion-item" style={{backgroundColor:props.mode==='dark'?'#2f2b2cff':'rgba(5, 107, 147, 0)' , color:props.mode==='dark'?'white':'black'}}>
+        <div className="accordion-item" style={{backgroundColor:props.mode==='dark'?'#191a1bff':'rgba(5, 107, 147, 0)' , color:props.mode==='dark'?'white':'black'}}>
             <h2 className="accordion-header">
-            <button className="accordion-button collapsed" type="button"  style={{backgroundColor:props.mode==='dark'?'#2f2b2cff':'rgba(5, 107, 147, 0)' , color:props.mode==='dark'?'white':'black'}} data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+            <button className="accordion-button collapsed" type="button"  style={{backgroundColor:props.mode==='dark'?'#191a1bff':'rgba(5, 107, 147, 0)' , color:props.mode==='dark'?'white':'black'}} data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                  <h5><strong> Free to Use</strong></h5>
             </button>
             </h2>
             <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
-            <div className="accordion-body" style={{backgroundColor:props.mode==='dark'?'#2f2b2cff':'rgba(5, 107, 147, 0)' , color:props.mode==='dark'?'white':'black'}}>
+            <div className="accordion-body" style={{backgroundColor:props.mode==='dark'?'#191a1bff':'rgba(5, 107, 147, 0)' , color:props.mode==='dark'?'white':'black'}}>
                   It is completely free to use. No credit cards required. Use as much as you want!!
             </div>
             </div>
         </div>
-        <div className="accordion-item" style={{backgroundColor:props.mode==='dark'?'#2f2b2cff':'rgba(5, 107, 147, 0)' , color:props.mode==='dark'?'white':'black'}}>
+        <div className="accordion-item" style={{backgroundColor:props.mode==='dark'?'#191a1bff':'rgba(5, 107, 147, 0)' , color:props.mode==='dark'?'white':'black'}}>
             <h2 className="accordion-header">
-            <button className="accordion-button collapsed" type="button"  style={{backgroundColor:props.mode==='dark'?'#2f2b2cff':'rgba(5, 107, 147, 0)' , color:props.mode==='dark'?'white':'black'}} data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+            <button className="accordion-button collapsed" type="button"  style={{backgroundColor:props.mode==='dark'?'#191a1bff':'rgba(5, 107, 147, 0)' , color:props.mode==='dark'?'white':'black'}} data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                  <h5><strong>Browser Compatible</strong></h5>
             </button>
             </h2>
             <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
-            <div className="accordion-body"  style={{backgroundColor:props.mode==='dark'?'#2f2b2cff':'rgba(5, 107, 147, 0)' , color:props.mode==='dark'?'white':'black'}}>
+            <div className="accordion-body"  style={{backgroundColor:props.mode==='dark'?'#191a1bff':'rgba(5, 107, 147, 0)' , color:props.mode==='dark'?'white':'black'}}>
                 It is compatible with all of your favorite browsers.
             </div>
             </div>
@@ -57,4 +57,5 @@
             </div>
         )
         }
+
 
