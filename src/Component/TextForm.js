f59bdd14a4b25858e6a43358684eb9a3e813f7e9
Akhilesh Kumar Sahu <118347309+mrakhil456@users.noreply.github.com>
2025-10-07 23:47:10 +0530

    Update TextForm.js

--- src/Component/TextForm.js
+++ src/Component/TextForm.js
@@ -46,11 +46,11 @@ export default function TextForm (props) {
 return (
     <>
     
-<div className='container'  style={{backgroundColor:props.mode==='dark'?'#2f2b2cff':'rgba(5, 107, 147, 0)' , color:props.mode==='dark'?'white':'black'}}>
+<div className='container'  style={{backgroundColor:props.mode==='dark'?'#191a1bff':'rgba(5, 107, 147, 0)' , color:props.mode==='dark'?'white':'black'}}>
      <h1 className='mb-3'>{props.heading}</h1>   
     <div className="mb-3" >
     
-    <textarea className="form-control" value={text} onChange={handleOnchange} style={{backgroundColor:props.mode==='dark'?'#3a2c31fb':' rgba(93, 153, 183, 1)' , color:props.mode==='dark'?'white':'black' , border:'black'}}  id="myBox" rows="8"></textarea>
+    <textarea className="form-control" value={text} onChange={handleOnchange} style={{backgroundColor:props.mode==='dark'?'#525454b8':' #ffffff94' , color:props.mode==='dark'?'white':'black' , border:'black'}}  id="myBox" rows="8"></textarea>
     </div>
     <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpperClick}>Convert to Uppercase</button>
     <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLowerClick}>Convert to Lowercase</button>
@@ -71,3 +71,4 @@ return (
 
 
  
+
